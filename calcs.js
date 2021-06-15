1d36c2c1eb4f474d84aeac21ad6d8a0c28e955e2
Chandramani Kumar <78583307+ChandramaniKumar786@users.noreply.github.com>
2021-06-16 02:01:55 +0530

    Add files via upload

---
+++ calcs.js
@@ -0,0 +1,9 @@
+function insert(num){
+	$('.calc-display').val($('.calc-display').val() + num) ;
+}
+function eql(){
+	$('.calc-display').val(eval($('.calc-display').val()));
+}
+function c(){
+	$('.calc-display').val(' ');
+}
